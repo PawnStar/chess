e3f805a4c610101682afb3a551592e01d23af965
Cole Erickson <pawnstar4@gmail.com>
2018-12-13 01:19:53 -0700

    Comments

--- js/lights.js
+++ js/lights.js
@@ -7,19 +7,7 @@ window.kae.createLights = function createLights(){
 
   var ambient = new THREE.AmbientLight( 0xffffff, .1 ); // soft white light
   ambient.name = 'ambientLight'
-
-  // var directionalLight = new THREE.PointLight( 0xffffff, .7 );
-  // directionalLight.position.set(15, 15, 0)
-  // directionalLight.castShadow = true
-  // directionalLight.shadowDarkness = .7
-  // directionalLight.shadowMapWidth = 1024;
-  // directionalLight.shadowMapHeight = 1024;
-  // directionalLight.name = 'directionalLight'
-
   lights.add(ambient)
-  // lights.add(directionalLight)
-
-  
 
   var spot = new THREE.SpotLight(0xFFFFFF);
   spot.castShadow = true
